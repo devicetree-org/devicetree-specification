981696921d7b2795ab4f88734bc4482023cb2409
Grant Likely <grant.likely@linaro.org>
2016-04-29 11:54:13 +0100

    Add Devicetree logo to html and pdf output
    Signed-off-by: Grant Likely <grant.likely@linaro.org>

--- source/conf.py
+++ source/conf.py
@@ -138,12 +138,12 @@ html_theme_path = [alabaster.get_path()]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = "devicetree-logo.png"
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = "devicetree-favicon.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -244,7 +244,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = "devicetree-logo.png"
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
