f08db57312ec4d37dec3978bd70c5960d00027f1
Stephen Boyd <stephen.boyd@linaro.org>
2017-08-07 16:39:17 -0700

    conf: Use the svg for html logo
    I couldn't get the pdf output to use the svg, but at least we can
    get the html output to.
    
    Signed-off-by: Stephen Boyd <stephen.boyd@linaro.org>
    Signed-off-by: Rob Herring <robh@kernel.org>

--- source/conf.py
+++ source/conf.py
@@ -150,7 +150,7 @@ html_theme_path = [alabaster.get_path()]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "devicetree-logo.png"
+html_logo = "devicetree-logo.svg"
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
