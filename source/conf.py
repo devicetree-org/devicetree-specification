c5fa6fa5b0ce3286368c9238e2ef7b2928d6ee9f
Grant Likely <grant.likely@arm.com>
2017-12-18 15:07:15 +0000

    Revert abbreviation of month name
    Commit 6a00ff2f, "Extract document version from git tag" changed the
    date annotation from full month to the abbreviation. Full month looks
    better on the title page, so revert that hunk of the change.
    
    Signed-off-by: Grant Likely <grant.likely@arm.com>

--- source/conf.py
+++ source/conf.py
@@ -83,7 +83,7 @@ language = None
 # non-false value, then it is used:
 #today = ''
 # Else, today_fmt is used as the format for a strftime call.
-today_fmt = '%d %b %Y'
+today_fmt = '%d %B %Y'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
