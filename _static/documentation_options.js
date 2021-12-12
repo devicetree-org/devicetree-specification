5e33392f2f40425fa61faeeb02eea370e35843d3
robherring <robherring@users.noreply.github.com>
2021-12-12 21:17:36 +0000

    deploy: 84c7557cb63410cd34a7c3f10f31229bdc7d341e

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.4-rc1',
+    VERSION: 'v0.4-rc1-1-g84c7557',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'singlehtml',
